314ea6adc4d45fef47a363113326bd8402082517
AdvanDias <advan_dias@hotmail.com>
2021-05-14 17:07:32 -0300

    iniciando projeto

---
+++ src/main/java/com/SpringBoot/Api/MessageBox/MessageBoxApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Api.MessageBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot/Api/MessageBox/MessageBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Api.MessageBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
