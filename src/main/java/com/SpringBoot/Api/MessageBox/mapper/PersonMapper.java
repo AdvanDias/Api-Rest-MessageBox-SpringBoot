b490d62e06a085040f3b346ecbe7149dc8a0a8a0
AdvanDias <advan_dias@hotmail.com>
2021-05-14 22:36:12 -0300

    Dependência MapStruct, para converter um objeto dto para uma entidade e uma entidade para um dto. Package mapper. Interface PersonMapper para que o mapper saiba mapear todos os dados toModel.

---
+++ src/main/java/com/SpringBoot/Api/MessageBox/mapper/PersonMapper.java
@@ -0,0 +1,19 @@
+package com.SpringBoot.Api.MessageBox.mapper;
+
+import com.SpringBoot.Api.MessageBox.dto.request.PersonDTO;
+import com.SpringBoot.Api.MessageBox.entites.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "data", source = "data", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+
+}
